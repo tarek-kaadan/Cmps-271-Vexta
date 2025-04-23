73c100aa953857be8631fc93c65d505db9001a21
Tarek Kaadan <65241698+tarek-kaadan@users.noreply.github.com>
2025-04-24 02:48:36 +0300

    fixes

--- front-end/src/config.tsx
+++ front-end/src/config.tsx
@@ -1,1 +1,6 @@
-export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+if (!backendUrl) {
+  throw new Error("VITE_BACKEND_URL is not defined in your .env file");
+}
+
+export const API_BASE_URL = backendUrl;
